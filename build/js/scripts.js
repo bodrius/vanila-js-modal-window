4a34b751adc75ac891c6d7461f9a3a6c1f5d8ce1
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-05-14 21:42:02 +0300

    final version

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,1 +1,51 @@
-"use strict";var getId=document.querySelector("#modal"),modal=document.querySelector(".toggle-modal"),buttonClose=document.querySelector(".modal-box__close-button"),buttonCancel=document.querySelector(".modal-box__button-cancel"),buttonUninstall=document.querySelector(".modal-box__button-sucsess"),backDrop=document.querySelector(".modal-box__backdrop"),hiden=document.querySelector(".modal-box");function showModal(){modal.classList.remove("toggle-modal"),window.addEventListener("keydown",closeModalPressEsc),buttonUninstall.addEventListener("click",closeModalWithAlert),buttonClose.addEventListener("click",closeModal),buttonCancel.addEventListener("click",closeModal),backDrop.addEventListener("click",closeModalClickToBackdrop)}function closeModalPressEsc(){27===event.keyCode&&(modal.classList.add("toggle-modal"),window.removeEventListener("keydown",closeModalPressEsc))}function closeModal(){modal.classList.add("toggle-modal"),buttonClose.removeEventListener("click",closeModal),buttonCancel.removeEventListener("click",closeModal)}function closeModalWithAlert(){setTimeout(function(){alert("DONE!!!")},300),modal.classList.add("toggle-modal")}function closeModalClickToBackdrop(e){e.target===e.currentTarget&&(modal.classList.add("toggle-modal"),backDrop.addEventListener("click",closeModalClickToBackdrop))}getId.addEventListener("click",showModal);
+"use strict";
+
+var getId = document.querySelector('#modal');
+var modal = document.querySelector('.modal-box');
+var buttonClose = document.querySelector('.modal-box__close-button');
+var buttonCancel = document.querySelector('.modal-box__button-cancel');
+var buttonUninstall = document.querySelector('.modal-box__button-sucsess');
+var backDrop = document.querySelector('.modal-box__backdrop');
+var hiden = document.querySelector('.modal-box');
+getId.addEventListener('click', showModal);
+
+function showModal() {
+  modal.classList.remove('close-modal');
+  modal.classList.add('toggle-modal');
+  window.addEventListener('keydown', closeModalPressEsc);
+  buttonUninstall.addEventListener('click', closeModalWithAlert);
+  buttonClose.addEventListener('click', closeModal);
+  buttonCancel.addEventListener('click', closeModal);
+  backDrop.addEventListener('click', closeModalClickToBackdrop);
+}
+
+function closeModalPressEsc() {
+  if (event.keyCode === 27) {
+    modal.classList.toggle('toggle-modal');
+    modal.classList.add('close-modal');
+    window.removeEventListener('keydown', closeModalPressEsc);
+  }
+}
+
+function closeModal() {
+  modal.classList.toggle('toggle-modal');
+  modal.classList.add('close-modal');
+  buttonClose.removeEventListener('click', closeModal);
+  buttonCancel.removeEventListener('click', closeModal);
+}
+
+function closeModalWithAlert() {
+  setTimeout(function () {
+    alert('DONE!!!');
+  }, 500);
+  modal.classList.toggle('toggle-modal');
+  modal.classList.add('close-modal');
+}
+
+function closeModalClickToBackdrop(event) {
+  if (event.target === event.currentTarget) {
+    modal.classList.toggle('toggle-modal');
+    modal.classList.add('close-modal');
+    backDrop.addEventListener('click', closeModalClickToBackdrop);
+  }
+}
--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,5 +1,5 @@
 const getId = document.querySelector('#modal');
-const modal = document.querySelector('.toggle-modal');
+const modal = document.querySelector('.modal-box');
 const buttonClose = document.querySelector('.modal-box__close-button');
 const buttonCancel = document.querySelector('.modal-box__button-cancel');
 const buttonUninstall = document.querySelector('.modal-box__button-sucsess');
@@ -9,7 +9,8 @@ const hiden = document.querySelector('.modal-box');
 getId.addEventListener('click', showModal);
 
 function showModal() {
-  modal.classList.remove('toggle-modal');
+  modal.classList.remove('close-modal');
+  modal.classList.add('toggle-modal');
   window.addEventListener('keydown', closeModalPressEsc);
   buttonUninstall.addEventListener('click', closeModalWithAlert);
   buttonClose.addEventListener('click', closeModal);
@@ -19,13 +20,15 @@ function showModal() {
 
 function closeModalPressEsc() {
   if (event.keyCode === 27) {
-    modal.classList.add('toggle-modal');
+    modal.classList.toggle('toggle-modal');
+    modal.classList.add('close-modal');
     window.removeEventListener('keydown', closeModalPressEsc);
   }
 }
 
 function closeModal() {
-  modal.classList.add('toggle-modal');
+  modal.classList.toggle('toggle-modal');
+  modal.classList.add('close-modal');
   buttonClose.removeEventListener('click', closeModal);
   buttonCancel.removeEventListener('click', closeModal);
 }
@@ -33,13 +36,15 @@ function closeModal() {
 function closeModalWithAlert() {
   setTimeout(() => {
     alert('DONE!!!');
-  }, 300);
-  modal.classList.add('toggle-modal');
+  }, 500);
+  modal.classList.toggle('toggle-modal');
+  modal.classList.add('close-modal');
 }
 
 function closeModalClickToBackdrop(event) {
   if (event.target === event.currentTarget) {
-    modal.classList.add('toggle-modal');
+    modal.classList.toggle('toggle-modal');
+    modal.classList.add('close-modal');
     backDrop.addEventListener('click', closeModalClickToBackdrop);
   }
 }
